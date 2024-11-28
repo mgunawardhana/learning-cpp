8c87455c36ffb0136354de98457503f59106a217
mgunawardhana <mrgunawardhana27368@gmail.com>
2024-11-28 12:01:45 +0530

    content update

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
         cout stand for character output */
 
 
-    /** simple two numbers swapping tasks without third variable */
+    /** simple two numbers swapping tasks without third variable
     int a = 1;
     int b = 2;
 
@@ -14,4 +14,38 @@ int main() {
 
     std::cout << b;
     return 0;
+
+    int file_size = 0;  //Snake case
+    int FileSize = 0;   //Pascal case
+    int fileSize = 0;   //Camel case
+    int iFileSize = 0;  //Hungarian notation (not recommended), i stands for integer (first letter of the data type)
+
+    */
+
+    /** Mathematicle Operations */
+    // int x = 10;
+    // int y = 3;
+    // int z = x / y; // 10 / 3 = 3
+    // std::cout << z;
+
+    // double a = 10;
+    // int b = 3;
+    // double c = a / b; // 10 / 3 = 3
+    // std::cout << c;
+
+    // int x = 10;
+    // int y = 3;
+    // int z = x % y; // 10 % 3 = 1
+    // std::cout << z;
+
+    // int x = 10;
+    // int y = x++; // y = 10, x = 11 first x is assigned to y then x is incremented
+    // std::cout << y << " " << x;
+
+    // int x = 10;
+    // int y = ++x; // y = 11, x = 11 first x is incremented then assigned to y
+    // std::cout << y << " " << x;
+
+    return 0;
+
 }
