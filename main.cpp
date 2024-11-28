8e19bb5691cbe5ac8e96d969488ba02f294f9b26
mgunawardhana <mrgunawardhana27368@gmail.com>
2024-11-28 10:58:46 +0530

    simple two numbers swapping tasks without third variable

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
-#include <iostream>
+#include <iostream> // this library is capable for input and output operations from the user
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    /** std stand for standard library
+        cout stand for character output */
+
+
+    /** simple two numbers swapping tasks without third variable */
+    int a = 1;
+    int b = 2;
+
+    a = a + b;
+    b = a - b;
+
+    std::cout << b;
     return 0;
 }
